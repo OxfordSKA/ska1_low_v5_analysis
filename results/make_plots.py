ad2bf14192de86f175db9d0812eb035b73158fc3
Fred Dulwich <fred.dulwich@oerc.ox.ac.uk>
2016-10-03 16:39:23 +0100

    Updated plotting script.

--- results/make_plots.py
+++ results/make_plots.py
@@ -8,7 +8,8 @@ import itertools
 
 
 def main():
-    marker = itertools.cycle(('v', 's', 'o'))
+    marker = itertools.cycle(('s', 'o'))
+    colours = ('green', 'orange', 'black', 'blue', 'red')
     num_models = 5
 
     # Load all text files into a nested dictionary.
@@ -18,66 +19,82 @@ def main():
     # Load reference SKA layout.
     f = join(cable_length_dir, b'ska1_v5_cables.txt')
     cables = numpy.loadtxt(f)
-    f = b'iantconfig/Iantconfig_text_results/ska1v5_snap.20x3.txt'
-    d = numpy.loadtxt(f, skiprows=1)
-    t = {'baseline': d[:, 0], 'psfrms': d[:, 1], 'uvgap': d[:, 2],
-         'unwrap_radius': cables[0], 'cable_length': cables[1],
-         'cable_length_delta': 0.0}
-    data['ska1v5'] = t
-    ref_cable_length = cables[1]
-    ref_psfrms = gmean(d[:, 1])
-    ref_uvgap = gmean(d[:, 2])
+    for length in ('snap', '4hr'):
+        f = b'iantconfig/Iantconfig_text_results/ska1v5_%s.20x3.txt' % length
+        d = numpy.loadtxt(f, skiprows=1)
+        t = {'baseline': d[:, 0],
+             'psfrms': d[:, 1],
+             'uvgap': d[:, 2],
+             'unwrap_radius': cables[0],
+             'cable_length': cables[1],
+             'cable_length_delta': 0.0
+             }
+        data['ska1v5_%s' % length] = t
 
     # Load test models.
     for i_model in range(num_models):
         f = join(cable_length_dir, b'model%02i_cables.txt' % (i_model + 1))
-        if isfile(f):
-            cables = numpy.loadtxt(f)
-        else:
-            cables = numpy.zeros([9, 2])
-        for i_unwrap in range(9):
-            f = b'iantconfig/Iantconfig_text_results/%02i%02i_snap.20x3.txt' % \
-                (i_model + 1, i_unwrap)
-            d = numpy.loadtxt(f, skiprows=1)
-            t = {'baseline': d[:, 0], 'psfrms': d[:, 1], 'uvgap': d[:, 2],
-                 'unwrap_radius': cables[i_unwrap, 0],
-                 'cable_length': cables[i_unwrap, 1],
-                 'cable_length_delta': cables[i_unwrap, 1] - ref_cable_length,
-                 'psfrms_ratio': gmean(d[:, 1]) / ref_psfrms,
-                 'uvgap_ratio': gmean(d[:, 2]) / ref_uvgap}
-            data['%02i%02i' % (i_model + 1, i_unwrap)] = t
+        cables = numpy.loadtxt(f) if isfile(f) else numpy.zeros([9, 2])
+        for length in ('snap', '4hr'):
+            ref = data['ska1v5_%s' % length]
+            for i_unwrap in range(9):
+                f = b'iantconfig/Iantconfig_text_results/%02i%02i_%s.20x3.txt' \
+                    % (i_model + 1, i_unwrap, length)
+                d = numpy.loadtxt(f, skiprows=1)
+                t = {'baseline': d[:, 0],
+                     'psfrms': d[:, 1],
+                     'uvgap': d[:, 2],
+                     'unwrap_radius': cables[i_unwrap, 0],
+                     'cable_length': cables[i_unwrap, 1],
+                     'cable_length_delta':
+                         cables[i_unwrap, 1] - ref['cable_length'],
+                     'psfrms_ratio': gmean(d[:, 1]) / gmean(ref['psfrms']),
+                     'uvgap_ratio': gmean(d[:, 2]) / gmean(ref['uvgap'])
+                     }
+                data['%02i%02i_%s' % (i_model + 1, i_unwrap, length)] = t
 
     # Plot UVGAP for individual models.
-    for i_model in range(num_models):
-        _, ax = plt.subplots(figsize=(8, 8))
-        for i_unwrap in range(9):
-            t = data['%02i%02i' % (i_model + 1, i_unwrap)]
-            ax.plot(t['baseline'], t['uvgap'], '-', marker=marker.next(),
-                    linewidth=2, markersize=10,
-                    label='Model %02i, unwrap %i' % (
-                    i_model + 1, i_unwrap + 1))
-        ax.legend(loc='best')
-        ax.set_xlabel('Baseline length (m)')
-        ax.set_ylabel('UVGAP')
-        plt.show()
+    # for i_model in range(num_models):
+    #     _, ax = plt.subplots(figsize=(8, 8))
+    #     for length in ('snap', '4hr'):
+    #         for i_unwrap in range(9):
+    #             t = data['%02i%02i_%s' % (i_model + 1, i_unwrap, length)]
+    #             ax.plot(t['baseline'], t['uvgap'], '-', marker=marker.next(),
+    #                     linewidth=2, markersize=8,
+    #                     label='Model %02i, unwrap %i, length %s' % (
+    #                     i_model + 1, i_unwrap + 1, length))
+    #         ax.legend(loc='best')
+    #         ax.set_xlabel('Baseline length (m)')
+    #         ax.set_ylabel('UVGAP')
+    #         plt.show()
 
     # Plot UVGAP as a function of difference in cable cost
     # for each model series.
     keuro_cost_per_km = 1
     fig, ax = plt.subplots(figsize=(8, 8))
     for i_model in range(num_models):
-        x = []
-        y = []
-        for i_unwrap in range(9):
-            t = data['%02i%02i' % (i_model + 1, i_unwrap)]
-            x.append((t['cable_length_delta'] / 1000) * keuro_cost_per_km)
-            y.append(t['uvgap_ratio'])
-        if numpy.sum(x) > 0:
-            ax.plot(x, y, '-', marker=marker.next(), linewidth=2, markersize=10,
-                    label='Model %i' % (i_model + 1))
-    ax.legend()
+        for length in ('snap', '4hr'):
+            x = []
+            y = []
+            for i_unwrap in range(9):
+                t = data['%02i%02i_%s' % (i_model + 1, i_unwrap, length)]
+                x.append((t['cable_length_delta'] / 1000) * keuro_cost_per_km)
+                y.append(t['uvgap_ratio'])
+            label = 'Model %i' % (i_model + 1)
+            if length == 'snap':
+                label += ', snapshot'
+            elif length == '4hr':
+                label += ', 4-hour'
+            linestyle = '--' if length == 'snap' else '-'
+            if numpy.sum(x) > 0:
+                ax.plot(x, y, linestyle, marker=marker.next(), linewidth=2,
+                        color=colours[i_model],
+                        markersize=8, label=label)
+    ax.legend(ncol=2, loc='best')
     ax.set_xlabel('Additional cost, thousands of Euros (worst case)')
     ax.set_ylabel('UVGAP Ratio')
+    ax.grid(b=True, which='major', color='darkgrey', linestyle='-')
+    ax.grid(b=True, which='minor', color='lightgrey', linestyle='-')
     ax.set_ylim((0, 1))
     plt.show()
     fig.savefig('uvgap_ratio_cost.png')
